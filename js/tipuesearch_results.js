f3cec106d1a0d459dc633d967b4454df7e6ca94b
Ron Scott-Adams <ron@tohuw.net>
2015-07-19 01:21:41 -0400

    Removed Tipue Integration
    I think I’ll give Lunr a go.

--- js/tipuesearch_results.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#tipue_search_input').tipuesearch({
-        'mode': 'json',
-        'contentLocation': '/js/searchcontent.js',
-        'show': {{ site.data.search.results_count }},
-        'showURL': false
-    });
-});
